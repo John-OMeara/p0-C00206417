3a9e68f89e927eb655ffea1ad592bb9025140bd6
John-OMeara <C00206417@itcarlow.ie>
2018-09-25 10:11:05 +0100

    Submission

--- modulo_first.js
+++ modulo_first.js
@@ -1,6 +1,9 @@
 /*! modulo_first v0.0.0 - MIT license */
 'use strict';
 
+// NAME:   John O'Meara
+// ID:     C00206417
+
 /*
 Name: modulo_first
 The first integer in the list is n. Replace each integer by itself mod n.
@@ -8,8 +11,14 @@ Sample Input: 4 2 8 3 9 4 10 5 11 6
 Sample Output: 0 2 0 3 1 0 2 1 3 2 
 */
 
-var modulo_first = function () {
-  // your code goes here
+var modulo_first = function (list) {
+  var n = list[0];
+
+  for(var i=0; i < list.length; i++){
+    list[i] = list[i]%n;
+  }
+  
+  return(list);
 }
 
 if ( typeof module !== "undefined" ) {
--- spec/modulo_first.js
+++ spec/modulo_first.js
@@ -12,12 +12,19 @@ describe('modulo_first()', function () {
   });
 
   it('gets modulo', function () {
-    expect(modulo_first(5)).to.equal(modulo_first(5%5));
+    expect(modulo_first([5])).to.deep.equal([5%5]);
   });
 
   it('does something else', function () {
-    expect(true).to.equal(false);
+    expect(modulo_first([2,3,4,5])).to.deep.equal([0,1,0,1]);
   });
 
-  // Add more assertions here
+  it('matches sample', function() {
+    expect(modulo_first([4,2,8,3,9,4,10,5,11,6])).to.deep.equal([0,2,0,3,1,0,2,1,3,2]);
+  })
+
+  // n%0 should be NaN
+  it('check zero', function() {
+    expect(modulo_first([0,6,3,8,4])).to.deep.equal([NaN, NaN, NaN, NaN, NaN]);
+  })
 });
